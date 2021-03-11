1cff1bf94fd3d55f6e3e6ee4c3f9f87116f01e18
SugarBlank <eneblank19@protonmail.com>
2021-03-10 21:04:07 -0500

    Create RunProgram.cs

---
+++ Hielo/RunProgram.cs
@@ -0,0 +1,15 @@
+
+namespace Hielo{
+    // Class that runs everything.
+    public class RunProgram
+    {
+        static void Main(string[] args)
+        {
+            //Calls the Menu Function.
+            MenuSetup menu = new MenuSetup();
+            //Runs the menu.
+            menu.mainMenu();
+        }
+        
+    }
+}
